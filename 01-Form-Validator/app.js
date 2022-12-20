f4d5ec627806963f62dd0f83038bc379c76a4503
Maurice Chevez <mauricechevez@gmail.com>
2022-12-19 21:42:58 -0500

    Work on Query Selectors

--- 01-Form-Validator/app.js
+++ 01-Form-Validator/app.js
@@ -1,13 +1,33 @@
 /* Global Variables */
 const form = document.getElementById('form');
+const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const passwordVerify = document.getElementById('password-verify');
 const submit = document.getElementById('submit');
 
-// Event Listener
-form.addEventListener('submit', function(e){
-    console.log('hello');
+/* ⚡️ Functions ⚡️ */
+function showError(element,message){
+    const formControl = element.parentElement;
+    formControl.className = `form-control error`;
+    // Edit the message
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
 
+function showSuccess(element){
+    const formControl = element.parentElement;
+    formControl.className = `form-control success`;
+}
+
+// 👂 Event Listener 👂
+form.addEventListener('submit', function(e){
+
+    // Validate Username
+    if(username.value === ""){
+        showError(username,'Username is missing');
+    } else {
+        showSuccess(username);
+    }
     e.preventDefault()
 })
