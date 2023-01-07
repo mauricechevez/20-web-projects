4599a92b89fc22de392f5dff04f661018f1f2b5c
Maurice Chevez <mauricechevez@gmail.com>
2023-01-07 09:20:54 -0500

    Project 6 Modal Menu Slider complete

--- 06-modal-menu-slider/app.js
+++ 06-modal-menu-slider/app.js
@@ -1,9 +1,23 @@
 /* Global Variables */
 const toggleBtn = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
 
 
 /* 👂👂👂  Event Listeners 👂👂👂 */
 toggleBtn.addEventListener('click', ()=>{
-    document.getElementsByTagName('nav')[0].style.transform = "translateX(0)";
-    // modal.style.transform = "translateX(100%)";
+    document.body.classList.toggle('show-nav');
 })
+// Hide the modal
+close.addEventListener('click',()=>{
+    modal.classList.toggle('show-modal');
+})
+
+// Show the modal
+open.addEventListener('click',()=>{
+    modal.classList.toggle('show-modal');
+})
+
+// Hide modal on outside region (opaque)
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false)
