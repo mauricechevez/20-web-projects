ba7c36b04fb7defedf49567f68fa11bac15faad0
Maurice Chevez <mauricechevez@gmail.com>
2023-08-07 16:29:54 -0400

    Project 19 complete

--- 19-sortable-list/app.js
+++ 19-sortable-list/app.js
@@ -21,20 +21,40 @@ let dragStartIndex;
 
 // init
 createList();
+addEventListeners();
+
+// Check if items are in correct order
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector('.draggable').innerText.trim();
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add('wrong-answer');
+      listItem.lastElementChild.classList.add('wrong');
+    } else {
+      listItem.classList.remove('wrong-answer');
+      listItem.lastElementChild.classList.remove('wrong');
+      listItem.classList.add('right-answer');
+      listItem.lastElementChild.classList.add('right');
+    }
+  });
+}
 
 // Insert list items into the DOM
 function createList() {
   // spread operator will create a copy of the original, in this case
   [...richestPeople]
-  .map (p =>({value: p, sort:Math.random()}))
-  .sort(function(a,b){return a.sort - b.sort})
-  .map(a => a.value)
-  .forEach((person, index) => {
-    // Create a new LI element and insert into the DOM
-    const listItem = document.createElement('li');
-    // keep track of each item
-    listItem.setAttribute('data-index', index);
-    listItem.innerHTML = `<span class="number">${index + 1}
+    .map((p) => ({ value: p, sort: Math.random() }))
+    .sort(function (a, b) {
+      return a.sort - b.sort;
+    })
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      // Create a new LI element and insert into the DOM
+      const listItem = document.createElement('li');
+
+      // keep track of each item
+      listItem.setAttribute('data-index', index);
+      listItem.innerHTML = `<span class="number">${index + 1}
     </span>
     <div class="draggable" draggable="true">
     <p class="person-name">${person}</p>
@@ -42,7 +62,53 @@ function createList() {
 
     </div>
     `;
-    listItems.push(listItem);
-    draggableList.appendChild(listItem)
+      listItems.push(listItem);
+      draggableList.appendChild(listItem);
+    });
+}
+
+function addEventListeners() {
+  // get all Draggables
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
+  dragListItems.forEach((item) => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
   });
 }
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+function dragStart() {
+  // Get a value from the closest DOM element. The + makes it a number
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute('data-index');
+  // swap what we dragged
+  swapItems(dragStartIndex, dragEndIndex);
+  this.classList.remove('over');
+}
+function dragEnter() {
+  this.classList.add('over');
+}
+function dragLeave() {
+  //   console.log('dropped!');
+  this.classList.remove('over');
+}
+
+check.addEventListener('click', checkOrder);
